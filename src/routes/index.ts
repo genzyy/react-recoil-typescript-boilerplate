6ad881aeeced63f8a6ed38cc5a26a50dbdf0a9a8
Rishit <rishpandey8097@gmail.com>
2022-07-30 14:33:48 +0530

    add: secret page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,10 @@
 import { RouteType } from "interfaces/routes";
-import Home from "ui/application/home";
 
-export const authenticatedRoutes: Array<RouteType> = [];
+import Home from "ui/application/home";
+import UserDetails from "ui/application/user-details";
+
+export const authenticatedRoutes: Array<RouteType> = [
+    { path: "/secret", component: UserDetails },
+];
 
 export const publicRoutes: Array<RouteType> = [{ path: "/", component: Home }];
---
+++ src/ui/application/user-details/user-details.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from "react";
+
+const UserDetails: FC = () => {
+    return <p>User Details</p>;
+};
+
+export default UserDetails;
