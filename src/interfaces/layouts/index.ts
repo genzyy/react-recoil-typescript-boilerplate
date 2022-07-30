f7bd946e30bcc48fc5c00d4af68c06710ad6f9ed
Rishit <rishpandey8097@gmail.com>
2022-07-30 14:09:44 +0530

    setup: react helmet

--- src/interfaces/layouts/index.ts
+++ src/interfaces/layouts/index.ts
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
 export interface MainLayoutProps {
+    title?: string;
     children: ReactNode | ReactNode[];
 }
--- src/layouts/main/main.tsx
+++ src/layouts/main/main.tsx
@@ -1,11 +1,19 @@
 import React, { FC } from "react";
+import Helmet from "react-helmet";
 
 import { MainLayoutProps } from "interfaces";
 
 import styles from "./styles.module.scss";
 
-const MainLayout: FC<MainLayoutProps> = ({ children }) => {
-    return <div className={styles.mainLayout}>{children}</div>;
+const MainLayout: FC<MainLayoutProps> = ({ title = "React Cpp", children }) => {
+    return (
+        <div className={styles.mainLayout}>
+            <Helmet>
+                <title>{title}</title>
+            </Helmet>
+            <div>{children}</div>
+        </div>
+    );
 };
 
 export default MainLayout;
